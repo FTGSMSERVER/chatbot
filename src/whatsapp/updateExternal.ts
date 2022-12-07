846cfdbe87ee99da8e12818b3128129387da2f05
FTGSMSERVER <86839841+FTGSMSERVER@users.noreply.github.com>
2022-12-07 01:25:39 +0000

    Update updateExternal.ts

--- src/whatsapp/updateExternal.ts
+++ src/whatsapp/updateExternal.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 const updateStatusExternal = async (protocolo: string, status: string, link = "nao") => {
   try {
     if(link === "sim"){ 
-      return await axios.get(`http://ftgsmserver.com/php/wp.php?protocolo=${protocolo}&status=${status}&link=${link}`)
+      return await axios.get(`https://new.ftgsmserver.com/php/wp.php?protocolo=${protocolo}&status=${status}&link=${link}`)
   
     }
-    return await axios.get(`http://ftgsmserver.com/php/wp.php?protocolo=${protocolo}&status=${status}&link=`)
+    return await axios.get(`https://new.ftgsmserver.com/php/wp.php?protocolo=${protocolo}&status=${status}&link=`)
   
   } catch (error) {
     console.log(error)
